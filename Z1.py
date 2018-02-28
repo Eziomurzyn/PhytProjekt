7f3e521fd94c017dbca6993ebc7f63517c0e9f36
Łukasz Kowalczyk <36916040+Eziomurzyn@users.noreply.github.com>
2018-02-28 10:48:51 +0100

    Create Z1.py

---
+++ Z1.py
@@ -0,0 +1,8 @@
+name = input("Podaj imie")
+surname = input("Podaj nazwisko")
+age = input("Podaj wiek")
+print("Czesc ",name," ",surname);
+if(int(age)>17):
+  print("Jestes Pelnoletni")
+else:
+  print("Jestes niepelnoletni")
